58e3102d316cb2541c10b1e6b158228af1ccf09a
Landon Grindheim <landon.grindheim@gmail.com>
2021-08-19 12:17:56 -0400

    Override by default in CodeSpaces
    CodeSpaces runs ./install.sh when booting. There's no user to get a
    response from.

--- install.sh
+++ install.sh
@@ -26,7 +26,11 @@ install_dotfiles() {
     if [ -f "$destination" -o -d "$destination" -o -L "$destination" ]; then
       warning "⚠️  ~/$(basename $destination) already exists. Override? [y]es [n]o [q]uit"
 
-      read response
+      if [ "$CODESPACES" = "true" ]; then
+        response = "y"
+      else
+        read response
+      fi
 
       case "$response" in
         Y | y )
