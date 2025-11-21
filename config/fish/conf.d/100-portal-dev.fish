da4cc945dc444941777ea20edaf8bc8bc92454ec
Landon Grindheim <landon.grindheim@gmail.com>
2025-11-21 14:47:16 -0500

    Add Huntress-specific configuration

---
+++ config/fish/conf.d/100-portal-dev.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+# Homebrew
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+end
+
+# Mise (version manager)
+if command -v mise &> /dev/null
+    eval (mise activate fish)
+end
+
+# AWS Configuration
+set -gx AWS_PROFILE $USER
+set -gx AWS_VAULT_BACKEND keychain
+set -gx AWS_REGION us-east-1
