880120b4e2eadeb8bfb19a35373cf69784af961b
Landon Grindheim <landon.grindheim@gmail.com>
2025-11-30 16:29:36 -0500

    Update Tmux configuration to use Fish

--- config/fish/functions/tat.fish
+++ config/fish/functions/tat.fish
@@ -3,35 +3,24 @@
 # Attach or create tmux session named the same as current directory.
 
 function tat
-    set path_name (basename $PWD | tr . -)
-    set session_name $argv[1]
+    set -l path_name (basename $PWD | tr . -)
+    set -l session_name
 
-    if test -z "$session_name"
+    if test (count $argv) -gt 0
+        set session_name $argv[1]
+    else
         set session_name $path_name
     end
 
-    function not_in_tmux
-        test -z "$TMUX"
-    end
-
-    function session_exists
-        tmux list-sessions | sed -E 's/:.*$//' | grep -q "^$session_name\$"
-    end
-
-    function create_detached_session
-        env TMUX='' tmux new-session -Ad -s "$session_name"
-    end
-
-    function create_if_needed_and_attach
-        if not_in_tmux
-            tmux new-session -As "$session_name"
-        else
-            if not session_exists
-                create_detached_session
-            end
-            tmux switch-client -t "$session_name"
+    if test -z "$TMUX"
+        # Not in tmux, create new session or attach
+        tmux new-session -As "$session_name"
+    else
+        # In tmux, switch to session
+        if not tmux list-sessions | sed -E 's/:.*$//' | grep -q "^$session_name\$"
+            # Session doesn't exist, create it detached
+            env TMUX='' tmux new-session -Ad -s "$session_name"
         end
+        tmux switch-client -t "$session_name"
     end
-
-    create_if_needed_and_attach
 end
